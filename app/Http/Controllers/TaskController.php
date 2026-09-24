2008786ad90550913703e30abcedc0ec6e8fc123
curanalvin2-cpu <curanalvin2@gmail.com>
2026-09-24 20:10:51 +0000

    Final route and controller fixes for production

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,20 +7,17 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    // Display all tasks
     public function index()
     {
         $tasks = Task::all();
         return view('tasks.index', compact('tasks'));
     }
 
-    // Show form to create a new task
     public function create()
     {
         return view('tasks.create');
     }
 
-    // Store a newly created task
     public function store(Request $request)
     {
         $request->validate([
@@ -36,17 +33,15 @@ class TaskController extends Controller
             'due_date' => $request->due_date,
         ]);
 
-        return redirect('/tasks');
+        return redirect()->route('tasks.index');
     }
 
-    // Show form to edit a task
     public function edit($id)
     {
         $task = Task::findOrFail($id);
         return view('tasks.edit', compact('task'));
     }
 
-    // Update the task in database
     public function update(Request $request, $id)
     {
         $task = Task::findOrFail($id);
@@ -64,15 +59,14 @@ class TaskController extends Controller
             'due_date' => $request->due_date,
         ]);
 
-        return redirect('/tasks');
+        return redirect()->route('tasks.index');
     }
 
-    // Delete task
     public function destroy($id)
     {
         $task = Task::findOrFail($id);
         $task->delete();
 
-        return redirect('/tasks');
+        return redirect()->route('tasks.index');
     }
 }
